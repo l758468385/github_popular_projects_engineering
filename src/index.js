cb0bcd2d36411c9765da71c41381fd1166ef50a8
lirk <lirk@goktech.cn>
2024-06-25 22:50:25 +0800

    啊

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css'
 import App from "@/App";
-const it = 'Kevin  '
+const it = 'Kevin'
+const it2 = 'Kevin'
+const it3 = 'Kevin'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
