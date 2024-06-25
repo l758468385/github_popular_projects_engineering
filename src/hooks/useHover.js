a6fcd16c5e8ef5958a8113bc413f0218b1f796de
lirk <lirk@goktech.cn>
2024-06-25 22:46:50 +0800

    a

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function useHover() {
     const [hovering, setHovering] = React.useState(false)
 
-    const onMouseOver = () =>  setHovering(true)
+    const onMouseOver = () => setHovering(true)
     const onMouseOut = () => setHovering(false)
 
     return [hovering, { onMouseOver, onMouseOut }]
